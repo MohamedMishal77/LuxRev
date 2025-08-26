eb20671d56f9d8d5b34f3b29f4faaeed3f1e7b94
Mohamed Mishal M <mohamedmishal077@gmail.com>
2025-08-26 14:18:44 +0530

    errors handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './component/header.jsx';  
 import Hero from './component/Hero.jsx';
 import Fleet from './component/Fleet.jsx';
 import CarSpec from './component/Carspec.jsx';
 import Support from './component/Support.jsx';
-import Pricing from './component/pricing.jsx';
+import Pricing from './component/Pricing.jsx';
 import Footer from './component/Footer.jsx';
 function App() {
   
